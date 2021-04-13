d1161e07118b69af0d22e432233e39266437f6bc
juno3550 <78023734+juno3550@users.noreply.github.com>
2021-04-13 20:32:59 +0800

    生成测试报告并发送邮件

--- util/report_util.py
+++ util/report_util.py
@@ -11,5 +11,5 @@ def create_excel_report_and_send_email(excel_obj, receiver, subject, content):
     :return: 返回excel测试报告文件名
     """
     time_stamp = get_timestamp()
-    report_path = excel_obj.save(time_stamp)
+    report_path = excel_obj.save(subject, time_stamp)
     send_mail(report_path, receiver, subject+"_"+time_stamp, content)
